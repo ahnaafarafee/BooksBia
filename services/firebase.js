287ba071e3a4f3689ad3597f894d9d7ee2c6c7e7
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-02-15 20:44:37 +0600

    integreted local env variable

--- services/firebase.js
+++ services/firebase.js
@@ -1,15 +1,15 @@
 import firebase from "firebase/app";
 import "firebase/analytics";
-import "firebase/firestore"
+import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAl-C7_ouJYhK-OHIMaT_xZ-pp0J2yWz8",
-  authDomain: "booksiba-28d4a.firebaseapp.com",
-  projectId: "booksiba-28d4a",
-  storageBucket: "booksiba-28d4a.appspot.com",
-  messagingSenderId: "554423034832",
-  appId: "1:554423034832:web:02378dcc2276ffa6d3cfd5",
-  measurementId: "G-X0J5863QQW",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
