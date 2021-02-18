38bb3ed6f1204daafe9617c57555fabedccc6d38
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-02-18 22:17:58 +0600

    minor bugs resolved in the svg

--- components/MainContent/MainContentBooks.jsx
+++ components/MainContent/MainContentBooks.jsx
@@ -5,11 +5,11 @@ function MainContentBooks({ imageUrl, name, author }) {
     <>
       <div className="content__row--books">
         <Link href="/">
-          <a className="content__book-link">
-            <img src={imageUrl} alt="book" className="content__poster" />
+          <div className="content__book-link">
+            <img src={imageUrl} alt={name} className="content__poster" />
             <h4 className="content__heading--sub">{name}</h4>
             <h4 className="content__heading--sub">{author}</h4>
-          </a>
+          </div>
         </Link>
       </div>
     </>
