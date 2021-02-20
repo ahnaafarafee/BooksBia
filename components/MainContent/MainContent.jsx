13a28e175dbead78c39de254e9d41351e32bd8f1
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-02-20 21:16:26 +0600

    shuffled(randomize) books in page load

--- components/MainContent/MainContent.jsx
+++ components/MainContent/MainContent.jsx
@@ -9,11 +9,19 @@ function MainContent() {
   const db = firebase.firestore();
 
   const [books, setBooks] = useState([]);
+  const [shuffledBooks, setShuffledBooks] = useState([]);
   const [booksNewAdded, setBooksNewAdded] = useState([]);
 
   useEffect(() => {
     db.collection("books").onSnapshot((snapshot) =>
       setBooks(snapshot.docs.map((doc) => doc.data()))
+    );
+
+    setShuffledBooks(
+      books
+        .map((a) => ({ sort: Math.random(), value: a }))
+        .sort((a, b) => a.sort - b.sort)
+        .map((a) => a.value)
     );
 
     db.collection("books")
@@ -22,6 +30,9 @@ function MainContent() {
         setBooksNewAdded(snapshot.docs.map((doc) => doc.data()))
       );
   }, []);
+
+  console.log(shuffledBooks, "shuffled");
+  console.log(books, "unshaffled");
 
   return (
     <>
@@ -32,7 +43,7 @@ function MainContent() {
               <div className="content__box">
                 <h2 className="content__main-heading">Trending</h2>
                 <div className="content__row">
-                  {books.map((doc, index) => {
+                  {shuffledBooks.map((doc, index) => {
                     if (index <= 10) {
                       return (
                         <MainContentBooks
