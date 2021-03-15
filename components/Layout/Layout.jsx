f9536dbaa8830a8a247b1d1e7733a65cd93be5a9
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-03-16 00:42:57 +0600

    added on rout change loader

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,9 +1,27 @@
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import Router from "next/router";
+import nProgress from "nprogress";
+import Head from "next/head";
+
+Router.onRouteChangeStart = (url) => {
+  nProgress.start();
+};
+
+Router.onRouteChangeComplete = () => nProgress.done();
+Router.onRouteChangeError = () => nProgress.done();
 
 function Layout({ children }) {
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+        />
+      </Head>
       <Navbar />
       {children}
       <Footer />
