c12c1cee1d58e0ba17423b6023c1efa56da406c7
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-04-14 13:12:01 +0600

    updated fallback page styles

--- pages/fallback.js
+++ pages/fallback.js
@@ -6,11 +6,19 @@ export default function fallback() {
       <Head>
         <title>BooksBia | Download and Read Ebooks For Free</title>
       </Head>
-      <h1>Seems you're offline!</h1>
-      <h2>
-        Please make sure your internet connection to Download and Read Ebooks
-        For Free
-      </h2>
+      <div
+        style={{
+          textAlign: "center",
+          height: "100vh",
+          backgroundColor: "white",
+        }}
+      >
+        <h1>Seems you're offline!</h1>
+        <h2>
+          Please make sure your internet connection is okay to Download and Read
+          Ebooks For Free
+        </h2>
+      </div>
     </>
   );
 }
