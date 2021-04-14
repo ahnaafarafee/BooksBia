f76d0b5cd87bf16bf28235b09c4525f6deb03ca1
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-04-14 22:31:10 +0600

    updated stylings in fallback page

--- pages/fallback.js
+++ pages/fallback.js
@@ -6,19 +6,27 @@ export default function fallback() {
       <Head>
         <title>BooksBia | Download and Read Ebooks For Free</title>
       </Head>
-      <div
-        style={{
-          textAlign: "center",
-          height: "100vh",
-          backgroundColor: "white",
-        }}
-      >
+      <div className="main-page">
         <h1>Seems you're offline!</h1>
         <h2>
           Please make sure your internet connection is okay to Download and Read
           Ebooks For Free
         </h2>
       </div>
+      <style jsx>{`
+        .main-page{
+          background-color: white;
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          text-align: center;
+          align -items: center;
+          justify-content: center;
+           max-width: 800px;
+          margin: auto;
+          border: 1px solid rgb(199, 197, 197);
+        }
+      `}</style>
     </>
   );
 }
