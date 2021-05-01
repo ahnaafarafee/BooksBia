a313779f57b76d023721b2d74bd06c8b8e8b9ef0
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-05-01 13:56:26 +0600

    _app/js meta tag changes

--- pages/_app.js
+++ pages/_app.js
@@ -4,18 +4,17 @@ import Layout from "../components/Layout/Layout";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { library, config } from '@fortawesome/fontawesome-svg-core';
-import { faBorderAll, faList } from '@fortawesome/free-solid-svg-icons';
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faBorderAll, faList } from "@fortawesome/free-solid-svg-icons";
 
 config.autoAddCss = false;
 library.add(faList, faBorderAll);
 
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import "../styles/globals.scss";
 
 import { NewAddedBookProvider } from "../fetchData/context/NewAddedBookContext";
-
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -23,7 +22,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
       </Head>
       <NewAddedBookProvider>
