b91e0b08e9b5e52a4f6d08cc0f6f2aed758794cf
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2021-06-01 20:00:48 +0600

    updated favicons and logo

--- pages/_document.js
+++ pages/_document.js
@@ -7,10 +7,10 @@ class CustomDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="theme-color" content="#17202A" />
+          <meta name="theme-color" content="#F89880" />
           <meta charSet="utf-8" />
           <meta name="browsermode" content="application" />
-          <meta name="apple-mobile-web-app-title" content="BooksBia" />
+          <meta name="apple-mobile-web-app-title" content="Deyal" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
@@ -19,13 +19,12 @@ class CustomDocument extends Document {
 
           <meta
             name="description"
-            content="Read Bengali Books Online Free. BooksBia is online Bengali books reading library. You can download all Bengali books in PDF."
+            content="Read book reviews and awesome blog posts daily!"
           />
           <meta
             name="keywords"
             content="books, ebook, pdf, bangla, bengali books, humayun ahmed"
           />
-          {/* <link rel="canonical" href="https://localhost:3000"/> */}
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
@@ -40,77 +39,27 @@ class CustomDocument extends Document {
           />
           <link
             rel="apple-touch-icon"
-            sizes="57x57"
-            href="/icons/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/icons/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/icons/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/icons/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/icons/apple-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/icons/apple-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/icons/apple-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/apple-icon-152x152.png"
-          />
-          <link
-            rel="apple-touch-icon"
             sizes="180x180"
-            href="/icons/apple-icon-180x180.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/icons/android-icon-192x192.png"
+            href="icons/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/icons/favicon-96x96.png"
+            href="icons/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/icons/favicon-16x16.png"
+            href="icons/favicon-16x16.png"
           />
-          <meta
-            name="msapplication-TileImage"
-            content="icons/ms-icon-144x144.png"
+          <link
+            rel="mask-icon"
+            href="icons/safari-pinned-tab.svg"
+            color="#5bbad5"
           />
+          <meta name="msapplication-TileColor" content="#da532c" />
           <script
             defer
             src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
